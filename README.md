9ea421d844410c38a16488036a83879b0639f69c
zouyuanfeng <891178455@qq.com>
2017-07-07 15:55:32 +0800

    readme

--- README.md
+++ README.md
@@ -3,6 +3,63 @@
 * mysql数据库
 * nginx映射上传的图片目录
 
+#### sql
+```sql
+CREATE TABLE API (
+  id          INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  method      VARCHAR(30)     NOT NULL,
+  response    VARCHAR(1000)   NOT NULL,
+  groupname   VARCHAR(30)     NOT NULL,
+  description VARCHAR(1000)
+);
+```
+
+#### nginx配置
+安装
+```
+sudo apt-get install nginx
+```
+
+/etc/nginx/nginx.conf
+```
+修改user nobody --> user root
+```
+/etc/nginx/sites-available/default
+
+```
+server {
+        listen 8081 default_server;
+        listen [::]:8081 default_server;
+        # SSL configuration
+        #
+        # listen 443 ssl default_server;
+        # listen [::]:443 ssl default_server;
+        
+
+        #root /home/zou/tomcat/apache-tomcat-8.5.15/temp_img;
+
+        # 首页配置
+        index index.html index.htm index.nginx-debian.html;
+
+
+        server_name _;
+
+        access_log /home/zou/nginx.log;
+
+        #上传的文件路径映射             
+        location /pic {
+            alias /home/zou/tomcat/apache-tomcat-8.5.15/temp_img;
+        }
+        
+        location / {
+            proxy_pass http://192.168.11.105:8080/;
+        }
+}
+```
+
+
+
+
 #### 效果图
 * 主页
 
--- src/main/resources/sql/api.sql
+++ src/main/resources/sql/api.sql
@@ -1,35 +1,11 @@
 CREATE TABLE API (
-  id          INT PRIMARY KEY NOT NULL,
-  method      VARCHAR(30)     NOT NULL UNIQUE,
+  id          INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  method      VARCHAR(30)     NOT NULL,
   response    VARCHAR(1000)   NOT NULL,
+  groupname   VARCHAR(30)     NOT NULL,
   description VARCHAR(1000)
 );
 
-# 修改id为自增
-ALTER TABLE api.API
-  MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
 
 SELECT *
 FROM API;
-
-ALTER TABLE api.API ADD groupname VARCHAR(30) NOT NULL;
-
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test83', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test13', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test32', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test33', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test34', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test35', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test36', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test73', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test38', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test39', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test30', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test43', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test44', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test45', 'safdsfsda', 'dfsdfs');
-INSERT INTO API (.API.method, .API.response, .API.description) VALUES ('test56', 'safdsfsda', 'dfsdfs');
-
-UPDATE API SET groupname='test';
-
-SELECT DISTINCT API.groupname FROM API;
